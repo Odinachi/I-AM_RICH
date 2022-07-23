c6fd118204fc63baecf3a96f6584881237a21ff9
Odinachi David <odinachidavid@yahoo.com>
2022-07-23 10:28:32 +0100

    Initial Commit

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Odinachi David on 23/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
